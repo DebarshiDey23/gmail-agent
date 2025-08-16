4d051567c7f9eefc1cc81e32533c815b602ae5be
Debarshi Dey <ddey23@terpmail.umd.edu>
2025-08-15 22:39:55 -0700

    fixed firebase service import

--- gmail-agent-backend/src/db/firebase.ts
+++ gmail-agent-backend/src/db/firebase.ts
@@ -1,5 +1,6 @@
-import serviceAccount from "../config/firebase-service-key.json";
 import admin from "firebase-admin";
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_KEY as string);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
